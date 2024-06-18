157cb593007c1ba71021f74f09f117a3803cfa06
Aadarsh Kumar <123838450+Aadarsh032@users.noreply.github.com>
2024-06-18 19:57:23 +0530

    Update AddProduct.jsx

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -16,10 +16,9 @@ const AddProduct = () => {
         image: "",
     })
 
-    const imageHandler = (event) => {
-        setImage(event.target.files[0])
-
-    }
+    // const imageHandler = (event) => {
+    //     setImage(event.target.files[0])
+    // }
 
     const changeHandler = (event) => {
         setProductDetails({ ...productDetails, [event.target.name]: event.target.value });
@@ -29,25 +28,7 @@ const AddProduct = () => {
         let responseData;
         let product = productDetails;
 
-        let formData = new FormData();
-        formData.append('product', image);
-        //Connection String
-        await fetch('https://trendy-cart-backend.vercel.app/upload', {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-            },
-            body: formData,
-            credentials: 'include',
-        }).then((response) => response.json()).then((data) => {
-            responseData = data;
-        })
-
-        if (responseData.success) {
-            product.image = responseData.image_url;
-            console.log(product);
-             //Connection String
-            await fetch('https://trendy-cart-backend.vercel.app/addproduct', {
+          await fetch('https://trendy-cart-backend.vercel.app/addproduct', {
                 method: 'POST',
                 headers: {
                     Accept: 'application/json',
@@ -60,7 +41,40 @@ const AddProduct = () => {
                 data.success ? alert(`Product Added`) : alert("Failed");
 
             })
-        }
+
+        
+
+        // let formData = new FormData();
+        // formData.append('product', image);
+        // //Connection String
+        // await fetch('https://trendy-cart-backend.vercel.app/upload', {
+        //     method: 'POST',
+        //     headers: {
+        //         Accept: 'application/json',
+        //     },
+        //     body: formData,
+        //     credentials: 'include',
+        // }).then((response) => response.json()).then((data) => {
+        //     responseData = data;
+        // })
+
+        // if (responseData.success) {
+        //     product.image = responseData.image_url;
+        //     console.log(product);
+        //      //Connection String
+        //     await fetch('https://trendy-cart-backend.vercel.app/addproduct', {
+        //         method: 'POST',
+        //         headers: {
+        //             Accept: 'application/json',
+        //             'Content-Type': 'application/json',
+        //         },
+        //         body: JSON.stringify(product),
+        //         credentials: 'include',
+        //     }).then((response) => response.json()).then((data) => {
+
+        //         data.success ? alert(`Product Added`) : alert("Failed");
+        //     })
+        // }
 
     }
 
@@ -98,11 +112,11 @@ const AddProduct = () => {
                 <textarea value={productDetails.description_long} onChange={changeHandler} name='description_long' placeholder='Type Here' />
             </div>
             <div className="addproduct-itemfield">
-                <label htmlFor="file-input">
-                    {/* Logic to view the Image in the Upload Area Section */}
-                    <img src={image ? URL.createObjectURL(image) : upload_area} alt="" className='addproduct-thumnail-img' />
+                <label > URL
+                   // Logic to view the Image in the Upload Area Section
+{/*                     <img src={image ? URL.createObjectURL(image) : upload_area} alt="" className='addproduct-thumnail-img' /> */}
                 </label>
-                <input onChange={imageHandler} type="file" name='image' id='file-input' hidden />
+                <input value={productDetails.image} onChange={changeHandler} type="text" name='image' placeholder='Type Here' id='file-input'  />
             </div>
             <button onClick={() => Add_Product()} className='addproduct-btn'>ADD</button>
         </div>
