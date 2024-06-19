52b5c2c95ad4408de7c3ab38eb8939174c5288e6
Aadarsh Kumar <123838450+Aadarsh032@users.noreply.github.com>
2024-06-19 16:29:19 +0530

    Add files via upload

---
+++ frontend/src/Ibutton/Ibutton.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Ibutton.css'
+
+const Ibutton = () => {
+  return (
+   
+
+    <a href="https://aadarshportfolio.orangecom.xyz/" target="_blank"> <div id="ibutton">
+      <p id="insideibutton">
+        <i>i</i>    
+      </p>
+  </div></a>
+  )
+}
+
+export default Ibutton
